41a57a84bbe808e6854de045330ea73c85d0d3a8
Christian Pankratz <chrpank@googlemail.com>
2020-02-08 17:06:51 +0100

    added time scale function

---
+++ base_functions.c
@@ -0,0 +1,33 @@
+
+
+#include "base_functions.h"
+
+int calculate_time_scale(int year, int month, int day, float ut, float *days) {
+
+  if (year <= 1900 || year >= 2100) {
+    *days = 0;
+    return 0;
+  }
+
+  if (month < 1 || month > 12) {
+    *days = 0;
+    return 0;
+  }
+
+  if (day < 1 || day > 31) {
+    *days = 0;
+    return 0;
+  }
+
+  if (ut < 0 || ut > 24) {
+    *days = 0;
+    return 0;
+  }
+
+  *days = 367 * year - 7 * (year + (month + 9) / 12) / 4 + 275 * month / 9 +
+          day - 730530;
+
+  *days = *days + ut / 24.0;
+
+  return 1;
+}
---
+++ base_functions.h
@@ -0,0 +1,12 @@
+
+
+/**
+ * Calculate days from a date.
+ * param[in] year The year.
+ * param[in] month The month.
+ * param[in] day The day.
+ * param[in] ut The universal time.
+ * param[in/out] days The days.
+ * return not 0 if success, 0 otherwise.
+ */
+int calculate_time_scale(int year, int month, int day, float ut, float *days);
