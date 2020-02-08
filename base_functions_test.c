e37e9144363dd1e615ffee4d60434388b5404238
Christian Pankratz <chrpank@googlemail.com>
2020-02-08 22:47:42 +0100

    refine unit tests for timescale function

--- base_functions_test.c
+++ base_functions_test.c
@@ -5,46 +5,51 @@
 #include "base_functions.h"
 #include <acutest.h>
 
-void calculate_time_scale_day_zero_case_1(void) {
-  float *days;
+void calculate_time_scale_output_test(void) {
+  float days = 42;
 
-  calculate_time_scale(0, 0, 0, 0, days);
+  TEST_CHECK(calculate_time_scale(1999, 12, 31, 0.0, &days) != 0);
+  TEST_CHECK(days == 0.0);
+  TEST_MSG("days: %f", days);
+
+  TEST_CHECK(calculate_time_scale(1998, 12, 31, 24.0, &days) != 0);
+  TEST_CHECK(days == -365.0 + 1.0 /* 24h */);
+  TEST_MSG("days: %f", days);
+
+  TEST_CHECK(calculate_time_scale(2000, 12, 31, 24.0, &days) != 0);
+  TEST_CHECK(days == +365.0 + 1.0 /* leap year */ + 1.0 /* 24h */);
+  TEST_MSG("days: %f", days);
+
+  float days1 = 111.0;
+  float days2 = 222.0;
+  TEST_CHECK(calculate_time_scale(1999, 12, 31, 24.0, &days1) != 0);
+  TEST_CHECK(calculate_time_scale(2000, 01, 01, 00.0, &days2) != 0);
+  TEST_CHECK(days1 == days2);
+  TEST_MSG("days1: %f", days1);
+  TEST_MSG("days2: %f", days2);
 }
 
-void calculate_time_scale_day_zero_case_2(void) {
-  float *days;
+void calculate_time_scale_input_test(void) {
 
-  calculate_time_scale(0, 0, 0, 0, days);
+  float days;
+
+  TEST_CHECK(calculate_time_scale(2000, 1, 1, 1, &days) != 0);
+  TEST_CHECK(calculate_time_scale(1900, 1, 1, 1, &days) == 0);
+  TEST_CHECK(calculate_time_scale(2100, 1, 1, 1, &days) == 0);
+
+  TEST_CHECK(calculate_time_scale(2000, 10, 1, 1, &days) != 0);
+  TEST_CHECK(calculate_time_scale(2000, 00, 1, 1, &days) == 0);
+  TEST_CHECK(calculate_time_scale(2000, 13, 1, 1, &days) == 0);
+
+  TEST_CHECK(calculate_time_scale(2000, 10, 01, 1, &days) != 0);
+  TEST_CHECK(calculate_time_scale(2000, 10, 00, 1, &days) == 0);
+  TEST_CHECK(calculate_time_scale(2000, 10, 32, 1, &days) == 0);
+
+  TEST_CHECK(calculate_time_scale(2000, 10, 1, 10.0, &days) != 0);
+  TEST_CHECK(calculate_time_scale(2000, 10, 1, -0.1, &days) == 0);
+  TEST_CHECK(calculate_time_scale(2000, 10, 1, 24.1, &days) == 0);
 }
 
-void calculate_time_scale_false_input_year(void) {
-  float *days;
-
-  calculate_time_scale(0, 0, 0, 0, days);
-}
-
-void calculate_time_scale_false_input_month(void) {
-  float *days;
-
-  calculate_time_scale(0, 0, 0, 0, days);
-}
-
-void calculate_time_scale_false_input_day(void) {
-  float *days;
-
-  calculate_time_scale(0, 0, 0, 0, days);
-}
-
-void calculate_time_scale_false_input_ut(void) {
-  float *days;
-
-  calculate_time_scale(0, 0, 0, 0, days);
-}
-
-TEST_LIST = {{NULL, calculate_time_scale_day_zero_case_1},
-             {NULL, calculate_time_scale_day_zero_case_2},
-             {NULL, calculate_time_scale_false_input_year},
-             {NULL, calculate_time_scale_false_input_month},
-             {NULL, calculate_time_scale_false_input_day},
-             {NULL, calculate_time_scale_false_input_ut},
+TEST_LIST = {{NULL, calculate_time_scale_output_test},
+             {NULL, calculate_time_scale_input_test},
              {NULL, NULL}};
