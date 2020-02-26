3bd9a6ac64afdbb8c30927327413d61bf8838991
Christian Pankratz <chrpank@googlemail.com>
2020-02-26 19:22:15 +0100

    added function definition azimuthal coordinates

--- scenario_functions.h
+++ scenario_functions.h
@@ -1,3 +1,19 @@
 /**
  * Christian Pankratz 2020.
  */
+
+/**
+ * scenario azimuthal coordinates sun
+ * param[in] object the object
+ * param[in] year the year
+ * param[in] month the month
+ * param[in] day the day
+ * param[in] ut the universal time
+ * param[in] lat the local latitude
+ * param[in/out] az the azimuth
+ * param[in/out] alt the altitude
+ */
+int azimuthal_coordinates_for_date(const char *object, const int year,
+                                   const int month, const int day,
+                                   const float ut, const float lat, float *az,
+                                   float *alt);
