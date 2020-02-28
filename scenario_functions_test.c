3c829f62ed37a2913e13498acc6a59df73128a97
Christian Pankratz <chrpank@googlemail.com>
2020-02-28 20:07:31 +0100

    added scenario test for sun

--- scenario_functions_test.c
+++ scenario_functions_test.c
@@ -5,18 +5,76 @@
 #include "scenario_functions.h"
 
 #include <acutest.h>
+#include <math.h>
 
-void scenario_calculate_azimuthal_coordinates_test(void) {
+typedef struct {
+  float hour;
+  float minute;
+  float second;
+  int day;
+  int month;
+  int year;
+  float azi;
+  float alt;
+} obsdata;
 
-  float az, alt;
+void scenario_calculate_azimuthal_coordinates_sun_test(void) {
+  /*	Sun, lon 13.0 lat 52.0
+  -----------------------------------------
+          Date         |Horiz.coordinates |
+                       |(Azimuth/Altitude)|
+                       |                  |
+  ---------------------|------------------|
+    h  m  s            |    °       °     |
+  00 00 00  28.02.2010 | 013.932 -45.354  |
+  08 30 00  24.11.2012 | 145.768 +10.855  |
+  17 00 00  21.08.2015 | 275.324 +11.254  |
+  01 30 00  18.05.2018 | 034.848 -11.775  |
+  10 00 00  11.02.2021 | 158.478 +21.787  |
+  18 30 00  08.11.2023 | 278.968 -28.104  |
+  03 00 00  05.08.2026 | 053.222 -05.449  |
+  11 30 00  01.05.2029 | 190.000 +52.918  |
+  20 00 00  26.01.2032 | 291.171 -38.766  |
+  04 30 00  23.10.2034 | 092.735 -12.401  |
+  13 00 00  19.07.2037 | 223.236 +52.617  |
+  */
 
-  TEST_CHECK(scenario_calculate_azimuthal_coordinates(
-                 "sun", 2020, 2, 28, 12, 13.3, 52.45, &az, &alt) != 0);
+  obsdata data[] = {
+      {00.0, 00.0, 00.0, /**/ 28, /***/ 2, 2010, 013.932, -45.354},
+      {08.0, 30.0, 00.0, /**/ 24, /**/ 11, 2012, 145.768, +10.855},
+      {17.0, 00.0, 00.0, /**/ 21, /***/ 8, 2015, 275.324, +11.254},
+      {01.0, 30.0, 00.0, /**/ 18, /***/ 5, 2018, 034.848, -11.775},
+      {10.0, 00.0, 00.0, /**/ 11, /***/ 2, 2021, 158.478, +21.787},
+      {18.0, 30.0, 00.0, /***/ 8, /**/ 11, 2023, 278.968, -28.104},
+      {03.0, 00.0, 00.0, /***/ 5, /***/ 8, 2026, 053.222, -05.449},
+      {11.0, 30.0, 00.0, /***/ 1, /***/ 5, 2029, 190.000, +52.918},
+      {20.0, 00.0, 00.0, /**/ 26, /***/ 1, 2032, 291.171, -38.766},
+      {04.0, 30.0, 00.0, /**/ 23, /**/ 10, 2034, 092.735, -12.401},
+      {13.0, 00.0, 00.0, /**/ 19, /***/ 7, 2037, 223.236, +52.617}};
 
-  TEST_CHECK(1 == 0);
-  TEST_MSG("az: %f", az);
-  TEST_MSG("alt: %f", alt);
+  int num_data = 11;
+
+  float azi, alt;
+  float lon = 13.0;
+  float lat = 52.0;
+
+  int success = 1;
+
+  for (int index = 0; index < num_data; index++) {
+    TEST_CHECK(scenario_calculate_azimuthal_coordinates(
+                   "sun", data[index].year, data[index].month, data[index].day,
+                   data[index].hour + data[index].minute / 60.0 +
+                       data[index].second / 3600.0,
+                   lon, lat, &azi, &alt) != 0);
+
+    if (fabsf(data[index].azi - azi) > 1.0 ||
+        fabsf(data[index].alt - alt) > 1.0) {
+      success = 0;
+    }
+  }
+
+  TEST_CHECK(success == 1);
 }
 
-TEST_LIST = {{NULL, scenario_calculate_azimuthal_coordinates_test},
+TEST_LIST = {{NULL, scenario_calculate_azimuthal_coordinates_sun_test},
              {NULL, NULL}};
