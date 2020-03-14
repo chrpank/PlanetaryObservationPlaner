65103e14196c4458c67bec384b8620339b48a989
Christian Pankratz <chrpank@googlemail.com>
2020-03-14 11:06:28 +0100

    extended makefile and added test file

---
+++ date_manager_test.c
@@ -0,0 +1,11 @@
+/**
+ * Christian Pankratz 2020.
+ */
+
+#include "date_manager.h"
+#include <acutest.h>
+
+void template_test(void) { TEST_CHECK(1 == 1); }
+
+TEST_LIST = {{NULL, template_test}, //
+             {NULL, NULL}};
