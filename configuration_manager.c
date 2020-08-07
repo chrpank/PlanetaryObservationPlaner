069d429947a34b5e6362a90eaed2381cb5aa10bb
Christian Pankratz <chrpank@googlemail.com>
2020-08-07 18:35:19 +0200

    implemented load configuration file

--- configuration_manager.c
+++ configuration_manager.c
@@ -3,6 +3,8 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "configuration_manager.h"
 
@@ -10,9 +12,48 @@ static const int DEBUG_MODE_CONFIGURATION_MANAGER = 1;
 
 const char* CONFIG_FILE_PATH = "pop.cfg";
 
+const int index_lat = 0;
+const int index_lon = 1;
+const int index_utd = 2;
+float config_buffer[3];
+
 int load_configuration_file()
 {
-	return 0;
+	FILE* file;
+	int buffer_length = 40;
+	char line_buffer[buffer_length];
+
+	file = fopen(CONFIG_FILE_PATH, "r");
+
+	if(file == NULL)
+	{
+		printf("load_configuration_file: file open has failed");
+		return 0;
+	}
+
+	for(int index = 0; index < 3; index++)
+	{
+		fgets(line_buffer, buffer_length, file);
+		float value = strtof(line_buffer, NULL);
+
+		if(index == index_lat)
+		{
+			config_buffer[index_lat] = value;
+		}
+
+		if(index == index_lon)
+		{
+			config_buffer[index_lon] = value;
+		}
+
+		if(index == index_utd)
+		{
+			config_buffer[index_utd] = value;
+		}
+	}
+
+	fclose(file);
+	return 1;
 }
 
 int write_configuration_file()
